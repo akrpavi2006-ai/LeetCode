e5079fb341a65e25c68261710976ffc3b3cdd627
Pavithra K <akrpavi2006@gmail.com>
2026-08-20 11:10:04 +0530

    Time: 0 ms (100%), Space: 46.7 MB (30.93%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -18,7 +18,6 @@ public class Solution {
             fast=fast.next.next;
             if(slow==fast){
                 slow=head;
-
                 while(slow!=fast){
                     slow=slow.next;
                     fast=fast.next;
