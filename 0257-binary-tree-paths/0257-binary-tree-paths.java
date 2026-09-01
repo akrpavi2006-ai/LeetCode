d63645ee22c543946814a4be7c4f0c28c776466b
Pavithra K <akrpavi2006@gmail.com>
2026-09-01 15:05:41 +0530

    Time: 5 ms (34.95%), Space: 49.8 MB (28.98%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+ /*
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> result=new ArrayList<>();
@@ -34,5 +35,26 @@ class Solution {
         if (root.right != null) {
             findPaths(root.right, path + "->", result);
         }
+    }
+}
+*/
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> list = new ArrayList<>();
+        dfs(root, "", list);
+        return list;
+    }
+    void dfs(TreeNode root, String s, List<String> list) {
+        if (root == null) {
+            return;
+        }
+        s += root.val;
+        if (root.left == null && root.right == null) {
+            list.add(s);
+            return;
+        }
+        s = s + "->";
+        dfs(root.left, s, list);
+        dfs(root.right, s, list);
     }
 }
